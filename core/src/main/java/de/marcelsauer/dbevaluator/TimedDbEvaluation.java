872cdee24133bb3cb0148488edc402b6b4d3305f
Marcel Sauer <marcel.sauer@gmx.de>
2010-05-31 09:44:31 +0200

    new readme files and fixed pom issues

--- core/src/main/java/de/marcelsauer/dbevaluator/TimedDbEvaluation.java
+++ core/src/main/java/de/marcelsauer/dbevaluator/TimedDbEvaluation.java
@@ -12,14 +12,14 @@ import org.apache.log4j.NDC;
  * 
  * This file is part of DB Evaluator.
  * 
- * DB Evaluator is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or (at your option) any
- * later version.
+ * DB Evaluator is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * DB Evaluator is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * DB Evaluator is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
  * 
  * You should have received a copy of the GNU General Public License along with
@@ -28,29 +28,29 @@ import org.apache.log4j.NDC;
 public class TimedDbEvaluation implements DbEvaluation {
 
 	private static final Log log = LogFactory.getLog(TimedDbEvaluation.class);
-    public final List<SingleResult> singleResults = new ArrayList<SingleResult>();
-    public Result combinedResult;
+	public final List<SingleResult> singleResults = new ArrayList<SingleResult>();
+	public Result combinedResult;
 
-    private final StopWatch stopWatch;
-    private final DbEvaluation[] evaluations;
+	private final StopWatch stopWatch;
+	private final DbEvaluation[] evaluations;
 
-    public TimedDbEvaluation(DbEvaluation[] evaluations, StopWatch stopWatch) {
-        this.evaluations = evaluations;
-        this.stopWatch = stopWatch;
-    }
+	public TimedDbEvaluation(DbEvaluation[] evaluations, StopWatch stopWatch) {
+		this.evaluations = evaluations;
+		this.stopWatch = stopWatch;
+	}
 
-    @Override
-    public void run(LoggingCallback log) {
-        for (DbEvaluation evaluation : evaluations) {
-        	beforeRun(evaluation);
-            run(evaluation, log);
-            afterRun(evaluation);
-        }
-    }
+	@Override
+	public void run(LoggingCallback log) {
+		for (DbEvaluation evaluation : evaluations) {
+			beforeRun(evaluation);
+			run(evaluation, log);
+			afterRun(evaluation);
+		}
+	}
 
 	private void afterRun(DbEvaluation evaluation) {
 		NDC.pop();
-		
+
 		long durationTimeMillis = stopWatch.durationTimeMillis();
 		log.debug("finished  " + evaluation.getClass().getSimpleName() + " in " + durationTimeMillis + " (ms)");
 
@@ -72,20 +72,20 @@ public class TimedDbEvaluation implements DbEvaluation {
 	private void run(DbEvaluation evaluation, LoggingCallback log) {
 		stopWatch.reset();
 		stopWatch.start();
-		
+
 		evaluation.run(log);
-		
+
 		stopWatch.stop();
 	}
 
-    public class SingleResult {
-        public final Result result;
-        public final DbEvaluation evaluation;
+	public class SingleResult {
+		public final Result result;
+		public final DbEvaluation evaluation;
 
-        private SingleResult(Result result, DbEvaluation evaluation) {
-            this.result = result;
-            this.evaluation = evaluation;
-        }
-    }
+		private SingleResult(Result result, DbEvaluation evaluation) {
+			this.result = result;
+			this.evaluation = evaluation;
+		}
+	}
 
 }
